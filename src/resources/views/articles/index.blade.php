ddfb629a9f77eaa17f96ce482ab72660fb870bf9
CosmoMax10 <cosmomax10@gmail.com>
2022-12-20 18:55:43 +0900

    フロントに記事一覧をDBからとってきて表示。

--- src/resources/views/articles/index.blade.php
+++ src/resources/views/articles/index.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="/main.css">
 </head>
 <body>
     <header>
         <div class="site-title">ミニブログ</div>
     </header>
     <main class="container">
-        @foreach (@articles as $article)
+        @foreach ($articles as $article)
         <article class="article-item">
             <div class="article-title">{{ $article->title }}</div>
             <div class="article-body">{{ $article->body }}</div>
