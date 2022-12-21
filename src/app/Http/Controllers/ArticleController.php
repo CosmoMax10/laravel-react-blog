7af16bafd8b83d5b2bca54df8f5d527c5fcaaa76
CosmoMax10 <cosmomax10@gmail.com>
2022-12-21 10:29:51 +0900

    記事投稿機能実装

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -27,7 +27,9 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        $article = new Article();
+        $data = ['article' => $article];
+        return view('articles.create', $data);
     }
 
     /**
@@ -38,7 +40,16 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $article = new Article();
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+
+        return redirect(route('articles.index'));
     }
 
     /**
